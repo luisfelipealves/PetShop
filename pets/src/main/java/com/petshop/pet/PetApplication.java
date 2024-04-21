95f186b716c5ed5d8cc09ab2cfa61694fe89670d
luisfelipealves <luisfelipe.alves@gmail.com>
2024-04-21 19:15:57 +0100

    Making modules work under a project

--- pets/src/main/java/com/petshop/pet/PetApplication.java
+++ pets/src/main/java/com/petshop/pet/PetApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import common.audit.AuditAwareImpl;
 
 @SpringBootApplication
 @EnableFeignClients
